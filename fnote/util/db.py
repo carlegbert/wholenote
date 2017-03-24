f74c682157bea2dade4e083e669172710bb2b29b
Carl Egbert <egbertcarl@gmail.com>
2017-03-23 22:30:50 -0700

    change db script

--- fnote/util/db.py
+++ fnote/util/db.py
@@ -5,12 +5,14 @@ from fnote.extensions import db
 
 
 def init_db():
-    """
-    initialize prod & test databases
+    """ Initialize dev & test databases. Until project is closer to MVP,
+    dev db resets every time. This will eventually be called by a deployment
+    CLI rather than app.py.
     """
     if not database_exists(SQLALCHEMY_DATABASE_URI):
         create_database(SQLALCHEMY_DATABASE_URI)
-        db.create_all()
+    db.drop_all()
+    db.create_all()
 
     test_db_uri = '{0}_test'.format(SQLALCHEMY_DATABASE_URI)
     if not database_exists(test_db_uri):
