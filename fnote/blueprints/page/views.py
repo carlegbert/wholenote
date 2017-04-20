9a82f3a9444c09b5d7bce01d4ca2f7ceccabd9bd
Carl Egbert <egbertcarl@gmail.com>
2017-04-20 16:39:47 -0700

    fixes to settings.py.example

--- fnote/blueprints/page/views.py
+++ fnote/blueprints/page/views.py
@@ -8,4 +8,7 @@ page = Blueprint('page', __name__, template_folder='templates')
 @page.route('/index')
 @page.route('/home')
 def home():
+    # in prod this could return an SPA made with react, angular, etc,
+    # or the page blueprint could be deleted entirely and the static
+    # page could be served directly by nginx or apache.
     return render_template('page/index.html')
