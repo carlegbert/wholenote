a1dacbd506b10f865621da46b9ec1569f2695e93
Carl Egbert <egbertcarl@gmail.com>
2017-04-22 20:31:54 +0000

    change docker-compose.yml and db script for deployment

--- fnote/util/db.py
+++ fnote/util/db.py
@@ -5,14 +5,10 @@ from fnote.extensions import db
 
 
 def init_db():
-    """ Initialize dev & test databases. Until project is closer to MVP,
-    dev db resets every time. This will eventually be called by a deployment
-    CLI rather than app.py.
+    """ Initialize dev & test databases if necessary.
     """
     if not database_exists(SQLALCHEMY_DATABASE_URI):
         create_database(SQLALCHEMY_DATABASE_URI)
-    db.drop_all()
-    db.create_all()
 
     test_db_uri = '{0}_test'.format(SQLALCHEMY_DATABASE_URI)
     if not database_exists(test_db_uri):
